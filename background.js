05c85db798a4b5fe1b2551c1a1918afc3db87a1a
Karan Luthra <karanluthra06@gmail.com>
2013-05-10 01:53:38 +0530

    Major changes Both lookups make AJAX calls and phishtank is checked only once gsb turns negative. Secondly, instead of blocking request and popping up warning, warning is given as an alert, and then a new tab is opened containing the warning, which may be ignored. Thirdly, only mainframe and subframe requests are checked.

--- background.js
+++ background.js
@@ -1,57 +1,65 @@
+var redirectURL = "http://karanluthra.github.io/antiphish-extension/";
+
 chrome.webRequest.onBeforeRequest.addListener(
 	function(info) {
 	console.log(info.url);
-	var res = 0;
-	var test= encodeURIComponent(info.url);
-	var url = "http://checkurl.phishtank.com/checkurl/index.php?url=" + test;
-	var checkUrl = "https://sb-ssl.google.com/safebrowsing/api/lookup?client=chrome&apikey=ABQIAAAA-Hxlavz4z8NVViUnPvDL1BTK_nsfFWEX4t9Tt3Mbgn7PtyB-2g&appver=22.0&pver=3.0&url=" + test;
-	var ready=0;
-		
-		$.ajax({
-		url:url,
+	
+	var checkURL = encodeURIComponent(info.url);
+	var gsbURL = "https://sb-ssl.google.com/safebrowsing/api/lookup?client=chrome&apikey=ABQIAAAA-Hxlavz4z8NVViUnPvDL1BTK_nsfFWEX4t9Tt3Mbgn7PtyB-2g&appver=22.0&pver=3.0";
+
+	$.ajax({
+		url:gsbURL,
 		type:"POST",	
-		dataType:"xml",
-		success:function(xml){
-			
-			console.log(xml);
-
-			var results = $(xml).find("in_database");
-			console.log("Results: "+ results.text());
-			
-			if(results.text()==="true")
-			{
+		data: "1\n" + checkURL,
+		dataType:"text",
+		statusCode: {
+			200: function(){
+				console.log(info.url + " is a valid phish");
+				alert("Warning: This maybe a phishing site!");
 				
-				alert("PHISHING phtank");
-				res=1;
-				console.log("res is: " + res);
-				ready = 1;
-								
+				chrome.tabs.create({url: redirectURL});
+				},
+			204: function(checkURL){
+				console.log("gsb negative, checking phishtank");
+				var phtankURL = 'http://checkurl.phishtank.com/checkurl/';
+
+				$.ajax({
+					url:phtankURL,
+					type:"POST",	
+					data: { url : checkURL, app_key : '4ef7449b0aa8ee52ff4de753bf3bdf7f6cdd59a923a247af942ccdd56117e2e5'},
+					dataType:"xml",
+					success:function(xml){
+						
+						var results = $(xml).find("in_database");
+						
+						if(results.text()==="true"){
+
+							console.log(xml);
+							console.log("Results: "+ results.text());
+
+							alert("Warning: This maybe a phishing site!");
+							
+							chrome.tabs.create({url: redirectURL});	
+				
+							}
+			
+						}
+     					});
+
+
+				},
+
+			400: function(){
+				console.log("400: Bad Request — The HTTP request was not correctly formed");
+				},
+
+			503: function(){
+				console.log("503: Service Unavailable — The server cannot handle the request. Besides the normal server failures, it could also indicate that the client has been “throttled” by sending too many requests");	
+				}
+
+
 			}
-		}
-       });
-
-		var oReq = new XMLHttpRequest();
-		oReq.open("GET", checkUrl, false);
-		oReq.send(null);
-		if(oReq.status === 400){
-			console.log("Error status : " + oReq.status + "for the request :" + test);
-		}
-		else if(oReq.status === 200){
-				
-			alert("PHISHING gsb");
-			res=1;
-			console.log("res is: " + res);	
-			ready=1;
-		}
-		else if(oReq.status === 204){}
-		else{
-		console.log("Error status : " + oReq.status + "for the request :" + test);
-		}
-
-
-if(res===1 && ready===1){
-	return {redirectUrl: "http://akashgupta.my3gb.com/mainpage.html"};
-}
+     	});
 
 
 },
@@ -59,8 +67,7 @@ if(res===1 && ready===1){
   {
     urls: [
       "http://*/","https://*/","http://*/*","https://*/*"
-    ]
-  },
-  // extraInfoSpec
-  ["blocking"]);
+    ],
+    types : ["main_frame", "sub_frame"]
+  });
 
